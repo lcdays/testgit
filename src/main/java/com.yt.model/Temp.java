e37ed31d2c26f1b02e8c806f8ff54cc0dfa111bc
lcdays <lcdays@126.com>
2018-07-10 09:16:35 +0800

    lc

---
+++ src/main/java/com.yt.model/Temp.java
@@ -0,0 +1,21 @@
+package com.yt.model;
+
+public class Temp {
+    private String name;
+    private Integer age;
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public Temp() {
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
